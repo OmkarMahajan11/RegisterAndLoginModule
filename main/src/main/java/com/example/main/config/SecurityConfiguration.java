f844b248bfffd122b268f23ff1c6db3e3fcaaf54
Omkar Mahajan <omkar.mahajan@sarasanalytics.com>
2022-01-23 16:44:58 +0530

    login functionality

--- main/src/main/java/com/example/main/config/SecurityConfiguration.java
+++ main/src/main/java/com/example/main/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.example.main.config;
 
 import com.example.main.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,22 +15,23 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-  
-  @Autowired
-  private UserService userService;
 
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+  private UserService userService;
+  private BCryptPasswordEncoder passwordEncoder;
+
+  public SecurityConfiguration(UserService userService, BCryptPasswordEncoder passwordEncoder) {
+    this.userService = userService;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
     DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
     auth.setUserDetailsService(userService);
-    auth.setPasswordEncoder(passwordEncoder());
+    auth.setPasswordEncoder(passwordEncoder);
     return auth;
   }
+
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
--- main/src/main/java/com/example/main/service/UserServiceImpl.java
+++ main/src/main/java/com/example/main/service/UserServiceImpl.java
@@ -1,39 +1,55 @@
 package com.example.main.service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import com.example.main.dto.UserRegistrationDto;
 import com.example.main.model.Role;
 import com.example.main.model.User;
 import com.example.main.repository.UserRepository;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
   private UserRepository userRepository;
+  private BCryptPasswordEncoder passwordEncoder;
 
-  public UserServiceImpl(UserRepository userRepository) {
+  public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Override
   public User save(UserRegistrationDto urdto) {
     User user = new User(
       urdto.getFirstName(), urdto.getLastName(), 
-      urdto.getEmail(), urdto.getPassword(), 
+      urdto.getEmail(), passwordEncoder.encode(urdto.getPassword()), 
       Arrays.asList(new Role("ROLE_USER"))
-    );
-    
+    );    
     return userRepository.save(user);
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // TODO Auto-generated method stub
-    return null;
+    User user = userRepository.findByEmail(username);
+    if (user == null) {
+      throw new UsernameNotFoundException("Invalid username");
+    }
+    return new org.springframework.security.core.userdetails
+      .User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+  }
+
+  private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+    return roles.stream()
+      .map(r -> new SimpleGrantedAuthority(r.getName()))
+      .collect(Collectors.toList());
   }
 }
